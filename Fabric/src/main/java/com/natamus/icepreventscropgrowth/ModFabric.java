17ab1296793dd056338c2a95d9ff287ac0b8f1b3
Rick South <rick@natamus.com>
2023-12-12 09:51:33 +0100

    Ice Prevents Crop Growth source code for 1.20.1.

---
+++ Fabric/src/main/java/com/natamus/icepreventscropgrowth/ModFabric.java
@@ -0,0 +1,33 @@
+package com.natamus.icepreventscropgrowth;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.fabric.callbacks.CollectiveCropEvents;
+import com.natamus.icepreventscropgrowth.events.CropEvent;
+import com.natamus.icepreventscropgrowth.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CollectiveCropEvents.PRE_CROP_GROW.register((Level world, BlockPos pos, BlockState state) -> {
+			return CropEvent.onCropGrowth(world, pos, state);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
