5455db8363ef2cab3f599e88194a063dddf92b24
Rick South <rick@natamus.com>
2024-05-26 14:22:36 +0200

    (v3.3) (1.20.6 Neo) Updated mod to work with NeoForge version 20.6.78+.

--- NeoForge/src/main/java/com/natamus/icepreventscropgrowth/neoforge/events/NeoForgeCropEvent.java
+++ NeoForge/src/main/java/com/natamus/icepreventscropgrowth/neoforge/events/NeoForgeCropEvent.java
@@ -3,22 +3,21 @@ package com.natamus.icepreventscropgrowth.neoforge.events;
 import com.natamus.collective.functions.WorldFunctions;
 import com.natamus.icepreventscropgrowth.events.CropEvent;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.event.level.BlockEvent.CropGrowEvent;
-import net.neoforged.bus.api.Event.Result;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.level.block.CropGrowEvent;
 
 @EventBusSubscriber
 public class NeoForgeCropEvent {
 	@SubscribeEvent
-	public static void mobItemDrop(CropGrowEvent.Pre e) {
+	public static void onCropGrow(CropGrowEvent.Pre e) {
 		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
 		if (level == null) {
 			return;
 		}
 		
 		if (!CropEvent.onCropGrowth(level, e.getPos(), e.getState())) {
-			e.setResult(Result.DENY);
+			e.setResult(CropGrowEvent.Pre.Result.DO_NOT_GROW);
 		}
 	}
 }
