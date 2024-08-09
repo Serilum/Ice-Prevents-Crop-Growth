abfdfcdd1d080533bc478e6044732e0329040001
Rick South <rick@natamus.com>
2024-08-09 08:18:48 +0200

    Ice Prevents Crop Growth source code for 1.21.1.

---
+++ Common/src/main/java/com/natamus/icepreventscropgrowth/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.icepreventscropgrowth.util;
+
+public class Reference {
+	public static final String MOD_ID = "icepreventscropgrowth";
+	public static final String NAME = "Ice Prevents Crop Growth";
+	public static final String VERSION = "3.4";
+	public static final String ACCEPTED_VERSIONS = "[1.21.1]";
+}
