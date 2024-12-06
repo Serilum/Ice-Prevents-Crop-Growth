c1faaad6f0077b33ab25841925f480b23a8bcca9
Rick South <rick@natamus.com>
2024-12-06 13:26:41 +0100

    Ice Prevents Crop Growth source code for 1.21.4.

---
+++ Forge/src/main/java/com/natamus/icepreventscropgrowth/forge/events/ForgeCropEvent.java
@@ -0,0 +1,24 @@
+package com.natamus.icepreventscropgrowth.forge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.icepreventscropgrowth.events.CropEvent;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.level.BlockEvent.CropGrowEvent;
+import net.minecraftforge.eventbus.api.Event.Result;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeCropEvent {
+	@SubscribeEvent
+	public void mobItemDrop(CropGrowEvent.Pre e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+		
+		if (!CropEvent.onCropGrowth(level, e.getPos(), e.getState())) {
+			e.setResult(Result.DENY);
+		}
+	}
+}
