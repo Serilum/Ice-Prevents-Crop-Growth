cb24defed84be9858b683e8c1ce074bfd30262b6
Rick South <rick@natamus.com>
2025-07-01 10:11:56 +0200

    Ice Prevents Crop Growth source code for 1.21.7.

---
+++ Forge/src/main/java/com/natamus/icepreventscropgrowth/ModForge.java
@@ -0,0 +1,37 @@
+package com.natamus.icepreventscropgrowth;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.icepreventscropgrowth.forge.events.ForgeCropEvent;
+import com.natamus.icepreventscropgrowth.util.Reference;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge(FMLJavaModLoadingContext modLoadingContext) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		BusGroup busGroup = modLoadingContext.getModBusGroup();
+		FMLLoadCompleteEvent.getBus(busGroup).addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+    	ForgeCropEvent.registerEventsInBus();
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
