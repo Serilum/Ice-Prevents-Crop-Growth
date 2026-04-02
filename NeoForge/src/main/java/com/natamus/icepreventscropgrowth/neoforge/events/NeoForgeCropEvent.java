7f2cba406b67acd9861d155036eeaee7b11cc0f8
Rick South <rick@natamus.com>
2026-04-02 16:07:51 +0200

    Ice Prevents Crop Growth source code for 26.1.1.

---
+++ NeoForge/src/main/java/com/natamus/icepreventscropgrowth/neoforge/events/NeoForgeCropEvent.java
@@ -0,0 +1,21 @@
+package com.natamus.icepreventscropgrowth.neoforge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.icepreventscropgrowth.events.CropEvent;
+import net.minecraft.world.level.Level;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.level.block.CropGrowEvent;
+
+public class NeoForgeCropEvent {
+	@SubscribeEvent
+	public static void onCropGrow(CropGrowEvent.Pre e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+		
+		if (!CropEvent.onCropGrowth(level, e.getPos(), e.getState())) {
+			e.setResult(CropGrowEvent.Pre.Result.DO_NOT_GROW);
+		}
+	}
+}
